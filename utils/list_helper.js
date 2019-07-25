f259aae0255784a93a519f320e6bb5a87ea3d9d4
vprahaladhan <vprahaladhan@users.noreply.github.com>
2019-07-26 00:06:59 +0800

    part 4.5_tests_step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,24 @@ const dummy = blogs => {
 const totalLikes = blogs => {
     return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
-  
+
+const favoriteBlog = blogs => {
+    const favBlog = {
+        likes: 0,
+    }
+
+    const mostLikedBlog = blogs.reduce((total, blog) => {
+        console.log(total.likes, blog.likes)
+        return blog.likes > total.likes ? blog : total
+    }, favBlog)
+    
+    return {
+        title: mostLikedBlog.title,
+        author: mostLikedBlog.author,
+        likes: mostLikedBlog.likes
+    }
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
